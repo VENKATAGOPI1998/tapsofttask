c55381aa304b00db2e3fe0090be716e8fcf942fb
VENKATA GOPI PERAVARAPU <78922195+VENKATAGOPI1998@users.noreply.github.com>
2021-08-26 15:53:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Cart from "./Cart";
@@ -8,15 +7,9 @@ import Cart from "./Cart";
 function App() {
   return (
     <>
-      <Router>
           <div className="container">
-            <Switch>
-              <Route path="/" exact>
-                <Cart></Cart>
-              </Route>
-            </Switch>
+            <Cart></Cart>
           </div>
-      </Router>
     </>
   );
 }
